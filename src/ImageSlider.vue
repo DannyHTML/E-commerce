dcea54b63df8920adce8954b1500b733d662dab7
DannyHTML <dannyjag@gmail.com>
2023-10-08 18:00:51 +0200

    Made a click outside lightbox to remove lightbox while clicking outside the box

--- src/ImageSlider.vue
+++ src/ImageSlider.vue
@@ -48,63 +48,65 @@
       v-if="showLightbox"
       class="fixed inset-0 flex items-center justify-center bg-gray-700 bg-opacity-75"
     >
-      <div class="md:max-w-sm lg:max-w-md">
-        <div class="relative">
-          <img
-            class="object-contain md:rounded-xl"
-            :src="currentImage"
-            :alt="currentImageAlt"
-          />
-          <button class="absolute -top-12 right-0" @click="closeLightbox">
+      <div ref="lightbox">
+        <div class="md:max-w-sm lg:max-w-md">
+          <div class="relative">
             <img
-              class="h-5 w-5 brightness-200"
-              src="/images/icon-close.svg"
-              alt=""
+              class="object-contain md:rounded-xl"
+              :src="currentImage"
+              :alt="currentImageAlt"
             />
-          </button>
-
-          <img
-            v-for="(image, index) in images"
-            :key="index"
-            class="absolute left-0 top-0 rounded-xl object-contain opacity-0"
-            :src="image.src"
-            :alt="image.alt"
-            :class="{ 'opacity-100': index === currentIndex }"
-          />
-          <div
-            class="absolute top-1/2 flex w-full -translate-y-1/2 justify-between px-3"
-          >
-            <button
-              class="flex h-8 w-8 items-center justify-center rounded-full bg-white leading-none"
-              @click="previousSlide"
-            >
+            <button class="absolute -top-12 right-0" @click="closeLightbox">
               <img
-                class="-ml-1 h-4 w-3"
-                src="/images/icon-previous.svg"
+                class="h-5 w-5 brightness-200"
+                src="/images/icon-close.svg"
                 alt=""
               />
             </button>
-            <button
-              class="flex h-8 w-8 items-center justify-center rounded-full bg-white leading-none"
-              @click="nextSlide"
-            >
-              <img class="-mr-1 h-4 w-3" src="/images/icon-next.svg" alt="" />
-            </button>
-          </div>
-        </div>
-        <div class="mt-5 flex justify-center">
-          <div class="flex gap-4">
-            <div
+
+            <img
               v-for="(image, index) in images"
               :key="index"
-              class="mr-3 mt-4 h-20 w-20 hover:cursor-pointer hover:rounded-lg hover:outline hover:outline-primary-0"
-              @click="openLightbox(index)"
+              class="absolute left-0 top-0 rounded-xl object-contain opacity-0"
+              :src="image.src"
+              :alt="image.alt"
+              :class="{ 'opacity-100': index === currentIndex }"
+            />
+            <div
+              class="absolute top-1/2 flex w-full -translate-y-1/2 justify-between px-3"
             >
-              <img
-                class="rounded-md hover:opacity-40"
-                :src="image.src"
-                :alt="image.alt"
-              />
+              <button
+                class="flex h-8 w-8 items-center justify-center rounded-full bg-white leading-none"
+                @click="previousSlide"
+              >
+                <img
+                  class="-ml-1 h-4 w-3"
+                  src="/images/icon-previous.svg"
+                  alt=""
+                />
+              </button>
+              <button
+                class="flex h-8 w-8 items-center justify-center rounded-full bg-white leading-none"
+                @click="nextSlide"
+              >
+                <img class="-mr-1 h-4 w-3" src="/images/icon-next.svg" alt="" />
+              </button>
+            </div>
+          </div>
+          <div class="mt-5 flex justify-center">
+            <div class="flex gap-4">
+              <div
+                v-for="(image, index) in images"
+                :key="index"
+                class="mr-3 mt-4 h-20 w-20 hover:cursor-pointer hover:rounded-lg hover:outline hover:outline-primary-0"
+                @click="openLightbox(index)"
+              >
+                <img
+                  class="rounded-md hover:opacity-40"
+                  :src="image.src"
+                  :alt="image.alt"
+                />
+              </div>
             </div>
           </div>
         </div>
@@ -115,7 +117,16 @@
 
 <script setup lang="ts">
 import { ref, computed, onMounted, onUnmounted } from "vue";
-import { onClickOutside } from "@vueuse.core";
+import { onClickOutside } from "@vueuse/core";
+
+const lightbox = ref<null>(null);
+
+onMounted(() => {
+  onClickOutside(lightbox, () => {
+    closeLightbox();
+    console.log("test");
+  });
+});
 
 const currentIndex = ref<number>(0);
 
